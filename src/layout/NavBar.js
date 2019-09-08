366484fe455b304f4aa20dc31902802a0443b059
Harry C <harrychenhc@icloud.com>
2019-09-07 21:32:32 -0400

    initial commit

---
+++ src/layout/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const NavBar = props => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <h4>There will be a input drag bar to indicate how many elements are in the initial unsorted array</h4>
+        </li>
+        <li>
+          <h4>Bubble Sort</h4>
+        </li>
+        <li>
+          <h4>Selection Sort</h4>
+        </li>
+        <li>
+          <h4>Insertion Sort</h4>
+        </li>
+        <li>
+          <h4>Quick Sort</h4>
+        </li>
+        <li>
+          <h4>Heap Sort</h4>
+        </li>
+        <li>
+          <h4>Merge Sort</h4>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
